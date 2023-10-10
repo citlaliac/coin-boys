562a0f1e5049e7730e07953ba03cb1d380889fe3
Citlali <citlaliaguilar-canamar@macbook-pro.myfiosgateway.com>
2023-10-10 13:27:56 -0400

    pull out coin flipper and images to own files

---
+++ logic/coinFlipper.tsx
@@ -0,0 +1,20 @@
+import CoinType from "../assets/CoinTypes";
+
+function coinFlipper(): CoinType {
+    const randomNumber: number = Math.random();
+
+    //do coin logic
+    if (randomNumber < .995 && randomNumber >= .5) { // 500-994
+        return CoinType.headsCoin;
+    } if (randomNumber > .005 && randomNumber < .5) { // 6-499
+        return CoinType.tailsCoin;
+    } if (randomNumber >= .995) { // 995-100
+        return CoinType.headSideCoin;
+    } if (randomNumber <= .005) { //0-5
+        return CoinType.tailsSideCoin;
+    } else {
+        console.error('randomNumber value in coinFlipper was not set or value did not match any posible outcomes. Output of randomNumber: ' + randomNumber);
+    };
+}
+
+export default coinFlipper;
